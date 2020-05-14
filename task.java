31534e65eb2acc9fa63c50173942253891f6a2f2
Jaffar Abbas <gamaportal8@gmail.com>
2020-05-14 22:54:32 +0500

    14-5-2020

---
+++ task.java
@@ -0,0 +1,17 @@
+/**
+ * task
+ */
+
+
+class check{
+        static void m(){
+            int arr[] = new int[5];
+            System.out.println(arr[5]);
+        }
+}
+
+public class task{
+    public static void main(String[] args) {
+        check.m();
+    }
+}
