651ae7415b7a2bf510c1d26173ca74e9603f05d6
Jaffar Abbas <gamaportal8@gmail.com>
2020-05-19 03:09:41 +0500

    19-5-2020            3:09 AM

--- task.java
+++ task.java
@@ -4,14 +4,22 @@
 
 
 class check{
-        static void m(){
-            int arr[] = new int[5];
-            System.out.println(arr[5]);
+         public void one(int age){
+              if(age < 18){             
+                  throw new ArithmeticException("hello");
+              }
+         else{
+             System.out.println("no");
+         }
+           }
+         void m(){
+           one(15);
         }
 }
 
 public class task{
     public static void main(String[] args) {
-        check.m();
+        check obj = new check();
+        obj.m();
     }
 }
